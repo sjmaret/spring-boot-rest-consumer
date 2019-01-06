9cc4ba0b4a6f0b6dfd63864b3626a365e13fd0f6
Shannon Maret <sjmaret@gmail.com>
2019-01-06 17:31:58 -0600

    Spring REST Consumer

---
+++ src/main/java/com/spring/rest/SpringRestConsumerApplication.java
@@ -0,0 +1,38 @@
+package com.spring.rest;
+
+import com.spring.rest.domain.Quote;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.client.RestTemplateBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+@SpringBootApplication
+public class SpringRestConsumerApplication {
+
+	private static final Logger log = LoggerFactory.getLogger(SpringRestConsumerApplication.class);
+
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestConsumerApplication.class, args);
+	}
+
+	@Bean
+	public RestTemplate restTemplate(RestTemplateBuilder builder) {
+		return builder.build();
+	}
+
+	@Bean
+	public CommandLineRunner run(RestTemplate restTemplate) throws Exception {
+		return args -> {
+			Quote quote = restTemplate.getForObject(
+					"http://gturnquist-quoters.cfapps.io/api/random", Quote.class);
+			log.info(quote.toString());
+		};
+	}
+
+}
+
